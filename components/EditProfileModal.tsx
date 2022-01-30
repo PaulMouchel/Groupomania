1c9684f3ad51cb9539576720ef2ef95baa007359
PaulMouchel <paulmouchel@live.fr>
2022-01-30 21:07:45 +0400

    user name can be changed (but page steel needs to be actuliaze to change username in navbar)

--- components/EditProfileModal.tsx
+++ components/EditProfileModal.tsx
@@ -4,79 +4,84 @@ import ClickAwayListener from '@mui/material/ClickAwayListener'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Avatar from '@mui/material/Avatar'
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import api from '../api/axios'
 import EditModalProfileType from '../types/EditProfileModal'
 
 const EditProfileModal: FC<EditModalProfileType> = ({ user, closeModal }) => {
 
     const descriptionRef = useRef<HTMLInputElement>(null)
+    const nameRef = useRef<HTMLInputElement>(null)
 
     useEffect(() => {
-        if (descriptionRef && descriptionRef.current) {
-            descriptionRef.current.value = user.description
+        if (descriptionRef && descriptionRef.current && nameRef && nameRef.current && user) {
+            nameRef.current.value = user.name
+            descriptionRef.current.value = user.description || ""
         }
     }, [])
 
-    const changeDescription = async (e:React.FormEvent) => {
+    const changeData = async (e:React.FormEvent) => {
         e.preventDefault()
-        // if (currentUser) {
-            const userId = user.id
-            const description = descriptionRef?.current?.value
-            if (description !== "") {
-                console.log(description)
-                const userUpdate = { description:description }
-                try {
-                    const response = await api.patch(`/users/${userId}`, userUpdate, {
-                        headers: {
-                            "authorization": localStorage.getItem("token") ||""
-                        }
-                    })
-                    localStorage.setItem("user", JSON.stringify(response.data))
-                } catch (error:unknown) {
-                    if (typeof error === "string") {
-                        console.log(`Error: ${error}`)
-                    } else if (error instanceof Error) {
-                        console.log(`Error: ${(error as Error).message}`)
-                    }
+
+        const userId = user.id
+        const description = descriptionRef?.current?.value
+        const name = nameRef?.current?.value
+
+        const userUpdate = { description:description, name:name }
+        try {
+            const response = await api.patch(`/users/${userId}`, userUpdate, {
+                headers: {
+                    "authorization": localStorage.getItem("token") ||""
                 }
+            })
+            localStorage.setItem("user", JSON.stringify(response.data))
+            closeModal()
+        } catch (error:unknown) {
+            if (typeof error === "string") {
+                console.log(`Error: ${error}`)
+            } else if (error instanceof Error) {
+                console.log(`Error: ${(error as Error).message}`)
             }
-        // }
+        }
     }
 
     return (
         <div className={styles.view}>
-            <ClickAwayListener onClickAway={closeModal}>
+            <ClickAwayListener onClickAway={() => closeModal()}>
                 <div className={styles.container}>
 
                     <div className={styles.header}>
                         <div className={styles.userpic}>
-                            <Avatar alt={user ? user.name : "user pic"} src={user ? user.imageUrl : ""} sx={{ width: 250, height: 250 }} />
+                            <Avatar alt={user.name} src={user.imageUrl} sx={{ width: 250, height: 250 }} />
                         </div>
-                        <span className={styles.username}>{user && user.name}</span>
+
+                        
                     </div>
                     
-                    <div className={styles.description}>
 
-                        <form onSubmit={(e:React.FormEvent) => changeDescription(e)}>
-                            <TextField
-                                id="description"
-                                label="Description"
-                                placeholder="Un mot sur vous ?"
-                                multiline
-                                variant="standard"
-                                fullWidth
-                                inputRef={descriptionRef}
-                            />
-                            <div className={styles.actions}>
-                                <Button variant="contained" type="submit" >Modifier</Button>
-                                <Button variant="outlined">Annuler</Button>
-                            </div>
-                        </form>
-
-                    </div>
-
-                    
+                    <form className={styles.form} onSubmit={(e:React.FormEvent) => changeData(e)}>
+                        <TextField
+                            id="name"
+                            label="Nom"
+                            placeholder="Votre nom"
+                            variant="standard"
+                            fullWidth
+                            inputRef={nameRef}
+                        />
+                        <TextField
+                            id="description"
+                            label="Description"
+                            placeholder="Un mot sur vous ?"
+                            multiline
+                            variant="standard"
+                            fullWidth
+                            inputRef={descriptionRef}
+                        />
+                        <div className={styles.actions}>
+                            <Button variant="contained" type="submit" >Modifier</Button>
+                            <Button variant="outlined" onClick={() => closeModal()}>Annuler</Button>
+                        </div>
+                    </form>         
                 </div>
             </ClickAwayListener>
         </div>
