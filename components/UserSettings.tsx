d5ffd080b9e62314e5634a72d4ebe6065f55d45c
PaulMouchel <paulmouchel@live.fr>
2022-02-22 11:03:07 +0400

    add account creation date

--- components/UserSettings.tsx
+++ components/UserSettings.tsx
@@ -4,10 +4,13 @@ import Button from '@mui/material/Button'
 import Modal from './Modal'
 import DeleteProfile from './DeleteProfile'
 import IUserSettings from '../interfaces/IUserSettings'
+import { DateTime } from "luxon"
+import Typography from '@mui/material/Typography'
 
 const UserSettings: FC<IUserSettings> = ({ user }) => {
 
     const [ modalOpen, setModalOpen ] = useState<boolean>(false)
+    const when = DateTime.fromISO(user.createdAt.toString()).setLocale('fr').toLocaleString(DateTime.DATE_FULL)
 
     const closeModal = () => {
         setModalOpen(false)
@@ -15,6 +18,7 @@ const UserSettings: FC<IUserSettings> = ({ user }) => {
 
     return (
         <div className={styles.container}>
+            <Typography>Utilisateur depuis le {when}</Typography>
             <Button variant="contained" onClick={() => setModalOpen(true)} >Supprimer le compte</Button> 
             { modalOpen && 
                 <Modal closeModal={closeModal}>
